ba3a34257787d78967c9bf895386c0e24b4b93a3
fredenga <dengafred@gmail.com>
2025-07-19 15:03:52 -0700

    update API call

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,42 @@
 import Search from "./components/Search.tsx";
-import {useState} from "react";
+import {useEffect, useState} from "react";
+
+const API_BASE_URL = "https://api.themoviedb.org/3"
+
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+const API_OPTIONS = {
+    method: "GET",
+    headers: {
+        accept: "application/json",
+        Authorization: `Bearer ${API_KEY}`,
+    }
+}
 
 const App = () => {
     const [searchTerm, setSearchTerm] = useState('')
+    const [errorMessage, setErrorMessage] = useState('')
+
+    const fetchMovies = async () => {
+        try {
+            const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`
+            const response = await fetch(endpoint, API_OPTIONS)
+            if(!response.ok) {
+                throw new Error("Failed to fetch movies")
+            }
+
+            const data = await response.json()
+            console.log(data)
+        } catch (error) {
+            console.error(`Fetching movies error: ${error}`)
+            setErrorMessage("Error fetching movies, please try again later")
+        }
+    }
+
+    useEffect(() => {
+        fetchMovies()
+
+    }, [])
     return (
         <main>
             <div className="pattern" />
@@ -13,8 +47,12 @@ const App = () => {
                         Find <span className="text-gradient">Movies</span>
                          you'll enjoy without a hassle
                     </h1>
+                    <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
                 </header>
-                <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+                <section>
+                    <h2>All Movies</h2>
+                    {errorMessage && <p className="text-red-500">{errorMessage}</p>}
+                </section>
             </div>
         </main>
     );
